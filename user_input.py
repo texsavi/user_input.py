74b1d1fb33d55b9c4e88bab28a3e1c8e65351ad3
T3xsavi <97127722+texsavi@users.noreply.github.com>
2024-02-24 00:01:34 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=input("What is your name?... ")
+age=input("What is your age?... ")
+location=input("Where do you live?... ")
+
+print(f"{name} from {location}! The amazing {age} year old star captain to fly us today! ")
